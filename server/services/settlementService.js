d5bddaf335789a63998b9af7434f37142b1fd506
Kaustubh-404 <kaustubhp2003@gmail.com>
2025-08-04 14:29:03 +0530

    minor bug fix

--- server/services/settlementService.js
+++ server/services/settlementService.js
@@ -8,7 +8,7 @@ class AutoSettlementService {
     constructor() {
         this.provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
         this.relayerWallet = new ethers.Wallet(process.env.PRIVATE_KEY, this.provider);
-        this.contractAddress = "0x07AF95749d58d4f6288490a58AC9cae6f602b194";
+        this.contractAddress = "0xd16Da71C739Bf59E919De5f179809F47093528DD";
         this.contract = new Contract(this.contractAddress, CONTRACT.abi, this.relayerWallet);
         this.isRunning = false;
     }
